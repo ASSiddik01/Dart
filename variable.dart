95e50a4e8b077b64e0a124a1f7e8b01c154483b4
Shama <assiddik001@gmail.com>
2022-03-29 23:55:53 +0600

    types of variable

---
+++ variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  // 4 types of variable
+  String myName = 'Shama'; //string
+  int age = 23; //integer
+  double height = 5.7; // decimal
+  bool female = false; // boolen
+
+  print('This is string value $myName');
+  print('This is integer value $age');
+  print('This is decimal value $height');
+  print('This is boolen value $female');
+}
