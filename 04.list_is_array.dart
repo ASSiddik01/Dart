bdbfdc4b0daf3125f146116bb419921caed609e2
Shama <assiddik001@gmail.com>
2022-03-30 00:35:26 +0600

    list

---
+++ 04.list_is_array.dart
@@ -0,0 +1,12 @@
+void main() {
+  // List is the similler of an array
+
+  // Without data type
+  // List students = ['Shama', 'Shakib', 'Sunny']; 
+
+  // With data type
+  List <String> students = ['Shama', 'Shakib', 'Sunny'];
+  // print(students);
+  print(students[0]); //access by index number
+
+}
