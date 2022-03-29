547a95ae5823756d57b48b49e34cf3b04c08e402
Shama <assiddik001@gmail.com>
2022-03-30 00:15:12 +0600

    mathematical operations

---
+++ 02.mathematical_operator.dart
@@ -0,0 +1,24 @@
+void main() {
+  int a = 5;
+  int b = 2;
+
+  // Summation
+  int sum = a + b;
+  print('Sum = $sum');
+
+  // Substruction
+  int sub = a - b;
+  print('Sub = $sub');
+
+  // Multipication
+  int multi = a * b;
+  print('Multi = $multi');
+
+  // Division
+  double divi = (a / b);
+  print('Div = $divi');
+
+  // Modulus
+  int modulas = a % b;
+  print('Modulas = $modulas');
+}
