f0638b193ac57660b2c3c41718e9eb1c26198c02
Shama <assiddik001@gmail.com>
2022-03-30 00:23:35 +0600

    Conditional operator

---
+++ 03.conditional_operator.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Conditional Operator
+  // ==
+  // !=
+  // <
+  // >
+  // <=
+  // >=
+
+  String myName = 'Siddik';
+  if (myName == 'Shama') {
+    print('$myName is my friend');
+  } else {
+    print('$myName is not my friend');
+  }
+}
