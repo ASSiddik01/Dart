772a46d591739c09a2cd2b771bb75d8ee84ff1df
Shama <assiddik001@gmail.com>
2022-03-30 01:34:32 +0600

    nested_list and list of map

---
+++ 07.nested_list.dart
@@ -0,0 +1,21 @@
+void main() {
+  List<List<int>> matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  // print(matrix[1][0]);
+
+// list of map
+  List<Map> students = [
+    {'name': 'Shama', 'id': 26},
+    {'name': 'Shakib', 'id': 01},
+    {'name': 'Salman', 'id': 16}
+  ];
+
+  for (int i = 0; i < students.length; i++) {
+    print(students[i]['name']);
+    print(students[i]['id']);
+    print('=========');
+  }
+}
