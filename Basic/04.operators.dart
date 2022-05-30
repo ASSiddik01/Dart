815fe54dafb66eaec184ef6f1da23e773e5a0acc
Shama <assiddik001@gmail.com>
2022-05-31 01:37:48 +0600

    operators

---
+++ Basic/04.operators.dart
@@ -0,0 +1,7 @@
+void main() {
+  // operator process the operands
+  // arithmatic operator : +, -, *, /, %, ++, --
+  // relational operator : >, <, >=, <=, ==, !=
+  // assignment operator : ??= += -=, *=, /=
+  // logical operator : && || !
+}
