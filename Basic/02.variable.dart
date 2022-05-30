165829b4322e6963524d643ff6b32038b45d65c7
Shama <assiddik001@gmail.com>
2022-05-31 01:26:20 +0600

    variable

---
+++ Basic/02.variable.dart
@@ -0,0 +1,22 @@
+void main() {
+  // variable simplicty
+  var name = "Shama"; //without spilicity
+  String myName = "Shama"; //with spilicity
+  print(myName);
+
+// String concatination or join
+  String fName = "Abu";
+  String lName = "Shama";
+  String fullName = fName + " " + lName;
+  print(fullName);
+
+  //number
+
+  int a = 23;
+  int b = 20;
+  int c = 25;
+
+  print( a + b + c);
+  print( a - b - c);
+  print( (a * b) / c);
+}
