c8f450128199d48c7da79418db28ec52558df8bd
Shama <assiddik001@gmail.com>
2022-05-31 01:15:41 +0600

    data types

---
+++ Basic/01.data_types.dart
@@ -0,0 +1,27 @@
+void main() {
+
+  /*
+  Number
+  ------
+  Integer - non fractional value
+  Double - fractional value
+
+  String
+  ------
+  alphabetic chracter
+
+  Boolen
+  ------
+  ture false
+
+  List
+  ------
+  hold object
+
+  Map
+  -----
+  holde object by key and value pare
+  
+*/
+  
+}
