ca3ebdd5c1b66f54780594b9a7ed7518b0e16fe4
Shama <assiddik001@gmail.com>
2022-05-31 09:05:56 +0600

    dart

---
+++ Basic/11.map.dart
@@ -0,0 +1,8 @@
+void main() {
+  Map myProfile = {'name': 'Shama', 'age': 23, 'district': 'Chuadanga'};
+
+  // add on runtime
+  myProfile['occupation'] = 'students';
+  print(myProfile);
+  print(myProfile['age']);
+}
