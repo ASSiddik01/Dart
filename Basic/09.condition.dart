5a14ac5abf0440b9283939b7504d0ac2e8b93a05
Shama <assiddik001@gmail.com>
2022-05-31 08:02:43 +0600

    condition

---
+++ Basic/09.condition.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var age = 23;
+  if (age > 18) {
+    print("adult");
+  } else if (age < 10) {
+    print("child");
+  } else {
+    print("Invalid age");
+  }
+}
