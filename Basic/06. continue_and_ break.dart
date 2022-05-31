47494cfc360661f074affe658068ec5271f5213b
Shama <assiddik001@gmail.com>
2022-05-31 07:54:25 +0600

    break & continue

---
+++ Basic/06. continue_and_ break.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  for (var i = 0; i <= 10; i++) {
+    // continue = skip
+    if (i == 5) {
+      continue;
+    }
+
+    // Break
+  if (i == 8) {
+      break;
+    }
+
+    print(i);
+  }
+
+}
