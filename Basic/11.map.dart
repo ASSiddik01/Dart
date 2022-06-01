ad01ab37a6b136581e78ccb3448d07c94f78b000
Shama <assiddik001@gmail.com>
2022-06-01 12:07:19 +0600

    map

--- Basic/11.map.dart
+++ Basic/11.map.dart
@@ -1,10 +1,31 @@
 void main() {
-
   // main map
   Map myProfile = {'name': 'Shama', 'age': 23, 'district': 'Chuadanga'};
 
   // add value on runtime
   myProfile['occupation'] = 'students';
+  // print(myProfile);
+  print(myProfile['occupation']);
+
+  // print key
+  // print(myProfile.keys);
+
+  // print values
+  print(myProfile.values);
+
+  // print length
+  // print(myProfile.length);
+
+  // add multiple value
+  myProfile
+      .addAll({"fatherName": "Abu Bakkor Siddik", "motherName": "Monoara"});
+
+  print(myProfile.values);
+
+  // delete all value
+  // myProfile.clear();
+
+  // remove value
+  myProfile.remove("name");
   print(myProfile);
-  print(myProfile['age']);
 }
