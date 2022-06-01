ebf7468510f03269719440ee40c6c9b8a9e32ebc
Shama <assiddik001@gmail.com>
2022-06-01 12:49:54 +0600

    Generics

---
+++ Basic/16.generics.dart
@@ -0,0 +1,5 @@
+void main() {
+  // generics means declear the data type
+  // List<generics/data type> students = ["Shama", "Sunny", "Shakib",23];
+  List<String> students = ["Shama", "Sunny", "Shakib"];
+}
