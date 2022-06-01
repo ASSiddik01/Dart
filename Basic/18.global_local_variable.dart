24fd88d082aab159aa312ca4180ce8eeb4423284
Shama <assiddik001@gmail.com>
2022-06-01 13:10:31 +0600

    global or local variable

---
+++ Basic/18.global_local_variable.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {}
+
+class myClass {
+  // Global variable - which is out of method
+  var name = "Shama";
+  var age = 23;
+
+  void myHobby() {
+    // local variable - which is not to access outside the method or fucntion
+    var firstHobby = "Photography";
+
+  }
+}
