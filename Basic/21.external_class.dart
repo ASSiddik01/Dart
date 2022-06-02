528438883e638902b5f109ca9e151800b11c2966
Shama <assiddik001@gmail.com>
2022-06-02 12:36:04 +0600

    use external class

---
+++ Basic/21.external_class.dart
@@ -0,0 +1,6 @@
+import '21.myClass.dart';
+
+void main(List<String> args) {
+  var myObj = myClass();
+  myObj.sum(2, 5);
+}
---
+++ Basic/21.myClass.dart
@@ -0,0 +1,5 @@
+class myClass {
+  void sum(a, b) {
+    print(a+b);
+  }
+}
