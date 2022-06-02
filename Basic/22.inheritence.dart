f793260555c2146d01a5cba530b0b5b6fbe504f3
Shama <assiddik001@gmail.com>
2022-06-02 12:46:13 +0600

    inheritance

---
+++ Basic/22.inheritence.dart
@@ -0,0 +1,12 @@
+import '22.main_property_of_inheritance.dart';
+
+// inheritance extands
+class inheritance extends mainClass {}
+
+
+// Access inheritance property
+void main(List<String> args) {
+  var myObj = new inheritance();
+
+  myObj.squeare(2);
+}
---
+++ Basic/22.main_property_of_inheritance.dart
@@ -0,0 +1,9 @@
+class mainClass {
+  void squeare(x) {
+    print(x * x);
+  }
+
+  void name(userName) {
+    print(userName);
+  }
+}
